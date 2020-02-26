dfe9feebc25e05403acb3ec473a5230e1002df12
Fariz Hermawan <fariz.hermawan@traveloka.com>
2020-02-26 15:31:48 +0700

    Add support for timeboard_id from new datadog_dashboard block

--- variables.tf
+++ variables.tf
@@ -52,6 +52,12 @@ variable "timeboard_id" {
   description = "The timeboard id for this monitor"
 }
 
+variable "dashboard_id" {
+  type        = "string"
+  default     = ""
+  description = "The timeboard_id if you are using new datadog_dashboard block"
+}
+
 variable "recipients" {
   type        = "list"
   default     = []
